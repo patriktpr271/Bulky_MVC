2427a74ec9821f2f24ed6b1fcea2c61cd22c0ed8
Tima Patrik Richárd <timapatrik27@gmail.com>
2024-05-29 15:52:03 +0200

    Finished chapter 3

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+	public class Category
+	{
+		// Primary Key of the table
+		[Key]
+		public int Id { get; set; }
+		// Name of the category
+		[Required]
+		[MaxLength(30)]
+		[DisplayName("Category Name")]
+		public string Name { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1, 100, ErrorMessage = "Display Order must be between 1-100!")]
+		public int DisplayOrder { get; set; }
+
+
+	}
+}
